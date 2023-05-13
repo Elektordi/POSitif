19e34fac265f59f4aa0145f0c486cec28edec5ad
Guillaume Genty <elektordi@elektordi.net>
2023-05-13 23:12:38 +0200

    First version of ticket printing

--- tickets_printer/src/index.ts
+++ tickets_printer/src/index.ts
@@ -1,6 +1,13 @@
 import { io, Socket } from "socket.io-client";
+import escpos from "escpos";
+// @ts-ignore
+import USB from "escpos-usb";
 
-const SERVER_URL = "ws://127.0.0.1:1337";
+
+const device = new USB(0x04b8, 0x0202);
+const printer = new escpos.Printer(device, { encoding: '858' });
+
+const SERVER_URL = process.env.SERVER_URL || "ws://127.0.0.1:1337";
 
 console.log("Connecting...")
 const socket: Socket = io(SERVER_URL);
@@ -19,5 +26,23 @@ socket.on("disconnect", (reason) => {
 
 socket.on("ticket:create", (data) => {
   console.log(data)
-  // TODO
+  const text = data.contents;
+
+  device.open(function(error: any){
+    if(error) {
+      console.log(error);
+    } else {
+      printer.font('B');
+      printer.style('NORMAL');
+      printer.align('LT');
+      printer.size(1, 1);
+      text.split(/\n/).forEach( (e: string) => {
+        printer.text(e);
+      });
+      printer.newLine();
+      printer.cut();
+      printer.close();
+    }
+  });
 });
+
